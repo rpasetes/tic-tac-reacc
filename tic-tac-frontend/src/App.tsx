1e2368dd10b4d0dd94579874d1e178a967f91f65
rpasetes <russap01@gmail.co>
2025-09-22 11:59:43 -0400

    setting the stage

---
+++ tic-tac-frontend/src/App.tsx
@@ -0,0 +1,27 @@
+import './App.css'
+
+function App() {
+  return (
+    <>
+      <table className='grid'>
+        <tr>
+          <td>X</td>
+          <td>X</td>
+          <td>X</td>
+        </tr>
+        <tr>
+          <td>X</td>
+          <td>X</td>
+          <td>X</td>
+        </tr>
+        <tr>
+          <td>X</td>
+          <td>X</td>
+          <td>X</td>
+        </tr>
+      </table>
+    </>
+  )
+}
+
+export default App
