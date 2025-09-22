95f40f1d5afc6ae52c5e3d5282fd35c2c99c1b4e
rpasetes <russap01@gmail.co>
2025-09-22 17:16:24 -0400

    move validation

--- tic-tac-frontend/src/tictactoe.ts
+++ tic-tac-frontend/src/tictactoe.ts
@@ -1,7 +1,3 @@
-// type Player = 'X' | 'O'
-
-// (1617) changing board to be single array for winner logic
-// (1650) for simplicity sake, let's just not use type Player
 export type GameState = {
   nowPlaying: string,
   winner: string | undefined
@@ -14,16 +10,15 @@ export const initGameState = {
   board: ['', '', '', '', '', '', '', '', '']
 }
 
-// (1618) changing board means updating write logic
-// (1656) cool, now my move logic seems funky
-// (1659) the board's not updating...
-// (1700) LMAO changing board means updating frontend indexing
+// (1710) seems straightforward: start with win detect (done @ 1713)
+// then, cell occupy detect (DONE @ 1715)
 export const makeMove = (gamestate: GameState, row: number, col: number): GameState => {
+  if (gamestate.winner) { return gamestate }
   const boardCopy = gamestate.board
   const cell = 3 * row + col
-  console.log('now clicking cell', cell)
+
+  if (boardCopy[cell] != '') { return gamestate }
   boardCopy[cell] = gamestate.nowPlaying
-  console.log('making new board:', boardCopy)
 
   const newPlayer = gamestate.nowPlaying === 'X' ? 'O' : 'X'
 
@@ -38,11 +33,6 @@ export const makeMove = (gamestate: GameState, row: number, col: number): GameSt
   return newGameState
 }
 
-// (1622) let's be dumb and make a list of 3x3 win states
-// (1635) lmao set the function you're exporting with '='
-// (1640) took a breather to block out game logic / graphics
-// time to add console logs for debugging
-// (1653) oh i have to check that our grids are filled with a winner
 export const checkWinner = (currentBoard: string[]): string | undefined => {
   const winStates = [
     [0, 1, 2], 
@@ -55,20 +45,16 @@ export const checkWinner = (currentBoard: string[]): string | undefined => {
     [2, 4, 6]
   ]
 
-  // console.log('now checking for wins on board:', currentBoard)
   for (let i = 0; i < winStates.length; i++) {
     const win = winStates[i]
-    // console.log('now checking win:', win)
     if (currentBoard[win[0]] || currentBoard[win[1]] || currentBoard[win[2]]) {
       if (currentBoard[win[0]] === currentBoard[win[1]]) {
         if (currentBoard[win[1]] === currentBoard[win[2]]) {
-          console.log('detecting a win!')
           return currentBoard[win[0]]
         }
       }
     }
   }
 
-  console.log('no wins, keep playing...')
   return undefined
 }
