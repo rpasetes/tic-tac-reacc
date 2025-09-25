865a907bda2e88f08d5c994dc23292fd9b9f8654
rpasetes <russap01@gmail.co>
2025-09-25 18:12:10 -0400

    init query hook

---
+++ tic-tac-frontend/src/Game.tsx
@@ -0,0 +1,31 @@
+export function Game({ gameState, setGameState }) {
+  const board = gameState.board
+  const winner = gameState.winner
+
+  return (
+    <div>
+      <table className='grid'>
+        <tbody>
+          <tr>
+            <td onClick={() => setGameState(makeMove(gameState,0,0))}>{board[0]}</td>
+            <td onClick={() => setGameState(makeMove(gameState,0,1))}>{board[1]}</td>
+            <td onClick={() => setGameState(makeMove(gameState,0,2))}>{board[2]}</td>
+          </tr>
+          <tr>
+            <td onClick={() => setGameState(makeMove(gameState,1,0))}>{board[3]}</td>
+            <td onClick={() => setGameState(makeMove(gameState,1,1))}>{board[4]}</td>
+            <td onClick={() => setGameState(makeMove(gameState,1,2))}>{board[5]}</td>
+          </tr>
+          <tr>
+            <td onClick={() => setGameState(makeMove(gameState,2,0))}>{board[6]}</td>
+            <td onClick={() => setGameState(makeMove(gameState,2,1))}>{board[7]}</td>
+            <td onClick={() => setGameState(makeMove(gameState,2,2))}>{board[8]}</td>
+          </tr>
+        </tbody>
+      </table>
+      {winner 
+      ? <h1>{winner} WINS!</h1>
+      : <></>}
+    </div>
+  )
+}
